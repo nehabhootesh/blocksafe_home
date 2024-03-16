eb2a2f1b47fb5e46b8e3679768a34d9c196bff5d
Neha <117673110+nehabhootesh@users.noreply.github.com>
2024-03-16 08:49:35 +0530

    Update Clogin_script.js

--- blocksafe_home/public/Clogin_script.js
+++ blocksafe_home/public/Clogin_script.js
@@ -5,41 +5,6 @@ document.getElementById('loginForm').addEventListener('submit', async function(e
   // Get userId and walletAddress from input fields
   const userId = document.getElementById('userId').value;
   const walletAddress = document.getElementById('walletAddress').value;
-
-  try {
-      // Send a POST request to the server
-      const response = await fetch('/Clogin', {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({ userId, walletAddress })
-      });
-
-      // Parse the JSON response
-      const data = await response.json();
-      console.log('data is available');
-
-      // Check if the request was successful
-      if (response.ok) {
-          // Extract walletAddress from the response data
-          const walletAddress = data.walletAddress;
-
-          // Use the walletAddress as needed
-          console.log('Wallet Address:', walletAddress);
-
-          // Call signAndSaveMessage function with walletAddress
-          signAndSaveMessage(walletAddress);
-         
-      } else {
-          // Display error message
-          alert(data.message);
-      }
-  } catch (error) {
-      console.error('Error:', error);
-  }
-});
-
 const contractAddress = '0xCeD8E5d96da547Cb4C9f49C83159f128e4364484';
         const contractABI = [
             {
@@ -90,6 +55,41 @@ const contractAddress = '0xCeD8E5d96da547Cb4C9f49C83159f128e4364484';
                 alert(`Error signing and saving message for ${name}: ${error.message}`);
             }
         }
+
+  try {
+      // Send a POST request to the server
+      const response = await fetch('/Clogin', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ userId, walletAddress })
+      });
+
+      // Parse the JSON response
+      const data = await response.json();
+      console.log('data is available');
+
+      // Check if the request was successful
+      if (response.ok) {
+          // Extract walletAddress from the response data
+          const walletAddress = data.walletAddress;
+
+          // Use the walletAddress as needed
+          console.log('Wallet Address:', walletAddress);
+
+          // Call signAndSaveMessage function with walletAddress
+          signAndSaveMessage(walletAddress);
+         
+      } else {
+          // Display error message
+          alert(data.message);
+      }
+  } catch (error) {
+      console.error('Error:', error);
+  }
+});
+
 
 // Function to login to the dashboard
 
